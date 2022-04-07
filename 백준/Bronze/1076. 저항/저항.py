036a48cb8d7041aad6d0b8fd94e3f374b50c694d
ohtjqkd <ohtjqkd@gmail.com>
2022-04-07 11:53:33 +0900

    [Bronze II] Title: 저항, Time: 112 ms, Memory: 29200 KB -BaekjoonHub

---
+++ 백준/Bronze/1076. 저항/저항.py
@@ -0,0 +1,8 @@
+colors = ['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'violet', 'grey', 'white']
+col_dic = dict(list(zip(colors, range(len(colors)))))
+
+first = input()
+second = input()
+third = input()
+
+print((col_dic.get(first)*10+col_dic.get(second))*(10**col_dic.get(third)))

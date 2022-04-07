e679723a7089b792be35c215fdcbaa9cf135e8ba
ohtjqkd <ohtjqkd@gmail.com>
2022-04-07 11:51:42 +0900

    [Bronze I] Title: 명령 프롬프트, Time: 80 ms, Memory: 29200 KB -BaekjoonHub

---
+++ 백준/Bronze/1032. 명령 프롬프트/명령 프롬프트.py
@@ -0,0 +1,11 @@
+n = int(input())
+strings = [input() for _ in range(n)]
+ret = ""
+for i in range(len(strings[0])):
+    for j in range(len(strings)-1):
+        if strings[j][i] != strings[j+1][i]:
+            ret += "?"
+            break
+    else:
+        ret += strings[0][i]
+print(ret)

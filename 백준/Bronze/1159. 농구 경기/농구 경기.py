31a732a07bda42cc4637ac58c80992140d8567e0
ohtjqkd <ohtjqkd@gmail.com>
2022-04-10 22:13:25 +0900

    [Bronze II] Title: 농구 경기, Time: 72 ms, Memory: 29200 KB -BaekjoonHub

---
+++ 백준/Bronze/1159. 농구 경기/농구 경기.py
@@ -0,0 +1,8 @@
+N = int(input())
+name_cnt = dict()
+for i in range(N):
+    fam_name = input()
+    name_cnt[fam_name[0]] = name_cnt.get(fam_name[0], 0) + 1
+ret = ''.join(sorted([k for k, v in name_cnt.items() if v >= 5]))
+ret = "PREDAJA" if ret == '' else ret
+print(ret)

436a4d447e5a6e7ad8021b46e22358a5008cb717
ohtjqkd <ohtjqkd@gmail.com>
2022-04-23 17:31:51 +0900

    [Bronze III] Title: Sequences, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/5101. Sequences/Sequences.py
+++ 백준/Bronze/5101. Sequences/Sequences.py
@@ -6,10 +6,7 @@ while True:
     s, d, n = map(int, input().rstrip().split(" "))
     if s == 0 and d == 0 and n == 0:
         break
-    if (n - s) % d == 0:
-        if (n - s) // d >= 0:
-            print((n - s) // d + 1)
-        else:
-            print("X")
+    if (n - s) % d == 0 and (n - s) // d >= 0:
+        print((n - s) // d + 1)
     else:
         print("X")

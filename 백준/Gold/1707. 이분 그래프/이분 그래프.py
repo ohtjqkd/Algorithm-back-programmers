ed50aa72e5b6a6013a40fabb3afcf319be0f4e5f
ohtjqkd <ohtjqkd@gmail.com>
2022-08-06 22:58:10 +0900

    [Gold IV] Title: 이분 그래프, Time: 1560 ms, Memory: 49544 KB -BaekjoonHub

---
+++ 백준/Gold/1707. 이분 그래프/이분 그래프.py
@@ -0,0 +1,36 @@
+import sys
+input = sys.stdin.readline
+K = int(input())
+
+def solution(edges, nodes, start):
+    color = 1
+    nodes[start] = color
+    stack = [(i, nodes[i])]
+    while stack:
+        curr_node, curr_color = stack.pop()
+        adjacents = edges[curr_node]
+        for a in adjacents:
+            if nodes[a] == curr_color:
+                return False
+            elif nodes[a] == 0:
+                nodes[a] = curr_color*(-1)
+                stack.append((a, nodes[a]))
+            else:
+                continue
+    return True
+
+for _ in range(K):
+    V, E = map(int, input().split(" "))
+    edges = [[] for _ in range(V)]
+    for _ in range(E):
+        n1, n2 = map(int, input().split(" "))
+        edges[n1-1].append(n2-1)
+        edges[n2-1].append(n1-1)
+    visited = [0 for _ in range(V)]
+    for i in range(V):
+        if visited[i] == 0:
+            if not solution(edges, visited, i):
+                print("NO")
+                break
+    else:
+        print("YES")

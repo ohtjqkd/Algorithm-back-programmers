b0db585aae6b7d22edde564132e598d967ddf2d1
ohtjqkd <ohtjqkd@gmail.com>
2022-08-06 22:49:30 +0900

    [Silver II] Title: 랜선 자르기, Time: 492 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
@@ -0,0 +1,21 @@
+K, N = tuple(map(int, input().split(" ")))
+lan = [int(input()) for _ in range(K)]
+total = sum(lan)
+left, right = 1, total//N
+while left < right:
+    # print(left, right)
+    S = 0
+    mid = (left + right) // 2
+    for l in lan:
+        S += l//mid
+    if S >= N:
+        left = mid+1
+    else:
+        right = mid-1
+S = 0
+for l in lan:
+    S += l//((left+right)//2)
+if S < N:
+    print((left+right)//2-1)
+else:
+    print((left+right)//2)

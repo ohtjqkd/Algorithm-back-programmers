d20768c5ee81f136ed3f66876bee1489434ed211
ohtjqkd <ohtjqkd@gmail.com>
2022-08-13 23:49:11 +0900

    [Silver I] Title: 트리 순회, Time: 68 ms, Memory: 29200 KB -BaekjoonHub

---
+++ 백준/Silver/1991. 트리 순회/트리 순회.py
@@ -0,0 +1,23 @@
+tree = {}
+def dfs(node, pre, ino, post):
+    if node == ".":
+        return
+    left, right = tree[node]["left"], tree[node]["right"]
+    pre.append(node)
+    dfs(left, pre, ino, post)
+    ino.append(node)
+    dfs(right, pre, ino, post)
+    post.append(node)
+    return pre, ino, post
+N = int(input())
+edge = []
+for _ in range(N):
+    edge.append(input().split(" "))
+    
+for p, l, r in edge:
+    tree[p] = {}
+    tree[p]["left"], tree[p]["right"] = l, r
+pre, ino, pos = dfs(edge[0][0], [], [], [])
+print(''.join(pre))
+print(''.join(ino))
+print(''.join(pos))

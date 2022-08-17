24093fe8e526c91fd6fbc88ac0bbf914c9c99cc8
ohtjqkd <ohtjqkd@gmail.com>
2022-08-17 21:15:35 +0900

    [Bronze II] Title: 거리의 합, Time: 72 ms, Memory: 30220 KB -BaekjoonHub

---
+++ 백준/Bronze/2399. 거리의 합/거리의 합.py
@@ -0,0 +1,8 @@
+n = int(input())
+
+location = list(map(int, input().split(" ")))
+location.sort()
+comb_n = [location[i]-location[i-1] for i in range(1, len(location))]
+comb_cnt = len(comb_n)
+part_cnt = [2 * (comb_cnt-i) * (i+1) for i in range(comb_cnt)]
+print(sum([comb_n[i]*part_cnt[i] for i in range(comb_cnt)]))

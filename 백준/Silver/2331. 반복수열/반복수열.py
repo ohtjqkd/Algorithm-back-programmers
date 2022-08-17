3e18c7045abd90bcbaabc1f525236050f0a694ae
ohtjqkd <ohtjqkd@gmail.com>
2022-08-17 21:15:46 +0900

    [Silver IV] Title: 반복수열, Time: 68 ms, Memory: 29200 KB -BaekjoonHub

---
+++ 백준/Silver/2331. 반복수열/반복수열.py
@@ -0,0 +1,15 @@
+A, P = map(int, input().split(" "))
+is_exit = {}
+node = A
+ret = []
+while is_exit.get(node) == None:
+    ret.append(node)
+    is_exit[node] = True
+    next_node = 0
+    while node > 0:
+        next_node += (node % 10) ** P
+        node //= 10
+    node = next_node
+while ret.pop() != node:
+    pass
+print(len(ret))

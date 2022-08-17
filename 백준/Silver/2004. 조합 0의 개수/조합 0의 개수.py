cfebad23258069016c9ee14f7465a18a979f122c
ohtjqkd <ohtjqkd@gmail.com>
2022-08-17 21:15:06 +0900

    [Silver II] Title: 조합 0의 개수, Time: 60 ms, Memory: 29380 KB -BaekjoonHub

---
+++ 백준/Silver/2004. 조합 0의 개수/조합 0의 개수.py
@@ -0,0 +1,26 @@
+n, m = map(int, input().split())
+def count_t(n):
+    answer = 0
+    i = 2
+    while i <= n:
+        answer += n//i
+        i = i*2
+    return answer
+
+def count_f(n):
+    answer = 0
+    i = 5
+    while i <= n:
+        answer += n//i
+        i = i*5
+    return answer
+two = 0
+five = 0
+two += count_t(n)
+five += count_f(n)
+two -= count_t(m)
+five -= count_f(m)
+two -= count_t(n-m)
+five -= count_f(n-m)
+
+print(min(two, five))

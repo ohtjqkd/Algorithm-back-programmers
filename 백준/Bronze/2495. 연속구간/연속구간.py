165a902572c79344339dd989847946ebc37d92b0
ohtjqkd <ohtjqkd@gmail.com>
2022-08-21 22:14:44 +0900

    [Bronze II] Title: 연속구간, Time: 68 ms, Memory: 29200 KB -BaekjoonHub

---
+++ 백준/Bronze/2495. 연속구간/연속구간.py
@@ -0,0 +1,10 @@
+tc = [input() for _ in range(3)]
+
+
+for t in tc:
+    max_l = 1
+    dp = [1 for _ in range(len(t))]
+    for i in range(1, len(t)):
+        if t[i] == t[i-1]:
+            dp[i] += dp[i-1]
+    print(max(dp))

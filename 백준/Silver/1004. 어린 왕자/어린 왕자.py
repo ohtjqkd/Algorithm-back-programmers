6664201d371b5a85e5f90ffe18ee0f05b9a0246a
ohtjqkd <ohtjqkd@gmail.com>
2023-09-27 14:02:20 +0900

    [Silver III] Title: 어린 왕자, Time: 228 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1004. 어린 왕자/어린 왕자.py
@@ -0,0 +1,16 @@
+T = int(input())
+
+for _ in range(T):
+  arrival_dest = list(map(int, input().split(' ')))
+  n = int(input())
+  include_circle = [False] * n
+  circle_info = []
+  for i in range(n):
+    circle_info.append(list(map(int, input().split(' '))))
+
+  for idx, circle in enumerate(circle_info):
+    if (arrival_dest[0] - circle[0]) ** 2 + (arrival_dest[1] - circle[1]) ** 2 < circle[2] ** 2:
+      include_circle[idx] = not include_circle[idx]
+    if (arrival_dest[2] - circle[0]) ** 2 + (arrival_dest[3] - circle[1]) ** 2 < circle[2] ** 2:
+      include_circle[idx] = not include_circle[idx]
+  print(include_circle.count(True))

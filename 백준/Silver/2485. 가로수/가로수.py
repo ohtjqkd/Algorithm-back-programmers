c547a80211b740eeacce2fe7728cfd892f9387fa
ohtjqkd <ohtjqkd@gmail.com>
2023-10-13 13:41:39 +0900

    [Silver IV] Title: 가로수, Time: 4088 ms, Memory: 34972 KB -BaekjoonHub

---
+++ 백준/Silver/2485. 가로수/가로수.py
@@ -0,0 +1,21 @@
+n = int(input())
+
+def gcd(a, b):
+  if b == 0:
+    return a
+  return gcd(b, a % b)
+
+prev_value = int(input())
+already_tree = [prev_value]
+dist = set()
+for i in range(n - 1):
+  curr_value = int(input())
+  already_tree.append(curr_value)
+  dist.add(curr_value - prev_value)
+  prev_value = curr_value
+
+dist = list(dist)
+a = dist[0]
+for i in range(1, len(dist)):
+  a = gcd(a, dist[i])
+print((already_tree[-1] - already_tree[0]) // a - n + 1)

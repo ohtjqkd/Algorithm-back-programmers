e1d6341a8d9764784a443c5e246ed8980277e4b0
ohtjqkd <ohtjqkd@gmail.com>
2023-10-17 00:23:41 +0900

    [Gold III] Title: 가장높은탑쌓기, Time: 56 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/2655. 가장높은탑쌓기/가장높은탑쌓기.py
@@ -0,0 +1,24 @@
+N = int(input())
+
+info = []
+info_dict = dict()
+for i in range(N):
+  a, h, w = map(int, input().split())
+  info.append((i, a, h, w))
+
+dp = [[info[i][2], [info[i][0]]] for i in range(N)]
+info.sort(key=lambda x: x[3])
+for i in range(len(info) - 1):
+  idx, a, h, w = info[i]
+  for j in range(i + 1, len(info)):
+    jdx, b, hh, ww = info[j]
+    if info[i][1] > info[j][1] or info[i][3] > info[j][3]:
+      continue
+    if dp[jdx][0] < dp[idx][0] + hh:
+      dp[jdx][0] = dp[idx][0] + hh
+      dp[jdx][1] = dp[idx][1] + [jdx]
+ret = max(dp, key=lambda x: x[0])
+print(len(ret[1]))
+for i in ret[1]:
+  print(i+1)
+  

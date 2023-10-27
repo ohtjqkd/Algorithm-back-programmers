50bf4bdbbf3b26c74396e1d2d0e52ecb43ddc835
ohtjqkd <ohtjqkd@gmail.com>
2023-10-27 21:48:14 +0900

    [Silver III] Title: 숫자 정사각형, Time: 64 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1051. 숫자 정사각형/숫자 정사각형.py
@@ -0,0 +1,12 @@
+n, m = map(int, input().split())
+board = [list(map(int, input())) for _ in range(n)]
+ans = 0
+for i in range(n):
+  for j in range(m):
+    a = board[i][j]
+    d = 0
+    while i + d < n and j + d < m:
+      if board[i + d][j] == a and board[i][j + d] == a and board[i + d][j + d] == a:
+        ans = max(ans, (d + 1) ** 2)
+      d += 1 
+print(ans)

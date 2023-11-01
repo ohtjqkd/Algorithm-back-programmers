69f1d02abc5f669e431243cdeab8256cd77f6b75
ohtjqkd <ohtjqkd@gmail.com>
2023-11-01 21:29:00 +0900

    [Silver IV] Title: 문자열, Time: 8 ms, Memory: 880 KB -BaekjoonHub

--- 백준/Silver/1120. 문자열/문자열.go
+++ 백준/Silver/1120. 문자열/문자열.go
@@ -2,30 +2,28 @@ package main
 
 import (
 	"fmt"
-	"sync"
 )
 
 func main() {
 	var a, b string
 
 	ans := 0
-	var wg sync.WaitGroup
 	fmt.Scan(&a, &b)
-	wg.Add(len(b) - len(a) + 1)
-	for i := 0; i < len(b)-len(a)+1; i++ {
-		go func(a, b string, si int, ans *int) {
-			defer wg.Done()
-			var count int
-			for i := 0; i < len(a); i++ {
-				if a[i] == b[i+si] {
-					count++
-				}
-			}
-			if *ans < count {
-				*ans = count
-			}
-		}(a, b, i, &ans)
+	for i := 0; i <= len(b)-len(a); i++ {
+		cnt := countSame(a, b, i)
+		if cnt > ans {
+			ans = cnt
+		}
 	}
-	wg.Wait()
 	fmt.Println(len(a) - ans)
+}
+
+func countSame(a, b string, si int) int {
+	var count int
+	for i := 0; i < len(a); i++ {
+		if a[i] == b[i+si] {
+			count++
+		}
+	}
+	return count
 }

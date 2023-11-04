abd9c9dd01328d67d51c0ddf10966a0cd88e7a86
ohtjqkd <ohtjqkd@gmail.com>
2023-11-04 23:10:00 +0900

    [Silver II] Title: 스택 수열, Time: 3772 ms, Memory: 36080 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.py
+++ 백준/Silver/1874. 스택 수열/스택 수열.py
@@ -1,24 +1,19 @@
+from collections import deque
+
 N = int(input())
-target = []
+target = deque([])
 for i in range(N):
-    target.append(int(input()))
+    target.appendleft(int(input()))
+stack, result = [], []
 
-numbers = [i for i in range(N, 0, -1)]
-stack = []
-result = []
-idx = 0
-while numbers or stack:
-    if stack and target[idx] == stack[-1]:
-        stack.pop()
+for i in range(1, N + 1):
+    stack.append(i)
+    result.append("+")
+    while stack and stack[-1] == target[-1]:
+        stack.pop(), target.pop()
         result.append("-")
-        idx += 1
-    else:
-        if not numbers:
-            result = ["NO"]
-            break
-        n = numbers.pop()
-        stack.append(n)
-        result.append("+")
-
-for i in result:
-    print(i)
+if len(target) > 0:
+    print("NO")
+else:
+    for sign in result:
+        print(sign)

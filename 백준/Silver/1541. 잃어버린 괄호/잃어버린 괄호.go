ecf6d31ac28c832bcffa78880375397066839a81
ohtjqkd <ohtjqkd@gmail.com>
2023-11-08 17:11:27 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 8 ms, Memory: 884 KB -BaekjoonHub

---
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	var s string
+	var e []string
+	fmt.Scan(&s)
+
+	// 다음에 slice에 append할 문자열의 첫번쨰 인덱스
+	curr := 0
+
+	// string slice로 분리
+	for i := 0; i < len(s); i++ {
+		// 연산자를 만난 경우
+		if isOperand(s[i]) {
+			if s[curr:i] != "" {
+				e = append(e, s[curr:i])
+			}
+			e = append(e, string(s[i]))
+			curr = i + 1
+		} else if i == len(s)-1 {
+			e = append(e, s[curr:i+1])
+		}
+	}
+
+	f := add
+	ans := 0
+	for i, v := range e {
+		if v == "-" {
+			f = sub
+		} else if v == "+" {
+			continue
+		} else {
+			a, _ := strconv.Atoi(e[i])
+			ans = f(ans, a)
+		}
+	}
+	fmt.Println(ans)
+}
+
+func isNum(c byte) bool {
+	return c >= '0' && c <= '9'
+}
+
+func isOperand(c byte) bool {
+	return c == '+' || c == '-'
+}
+
+func sub(a, b int) int {
+	return a - b
+}
+
+func add(a, b int) int {
+	return a + b
+}

2a2e169302ff156aec2e285bb844a4fac78847e9
ohtjqkd <ohtjqkd@gmail.com>
2023-11-08 20:15:26 +0900

    [Silver I] Title: 신입 사원, Time: 2468 ms, Memory: 39696 KB -BaekjoonHub

--- 백준/Silver/1946. 신입 사원/신입 사원.go
+++ 백준/Silver/1946. 신입 사원/신입 사원.go
@@ -17,15 +17,16 @@ func main() {
 
 	var t, n int
 	var a, b int
+	var scores [][]int
 
 	fmt.Fscanf(r, "%d\n", &t)
 	for i := 0; i < t; i++ {
-		var scores [][]int
 		ans := 0
 		fmt.Fscanf(r, "%d\n", &n)
+		scores = make([][]int, n)
 		for j := 0; j < n; j++ {
 			fmt.Fscanf(r, "%d %d\n", &a, &b)
-			scores = append(scores, []int{a, b})
+			scores[j] = []int{a, b}
 		}
 		sort.Slice(scores, func(i2, j int) bool {
 			return scores[i2][0] < scores[j][0]

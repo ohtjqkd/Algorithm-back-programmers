4e353f5267f26bf0225c48284ed6786978dcd1ac
ohtjqkd <ohtjqkd@gmail.com>
2023-11-08 20:32:45 +0900

    [Silver I] Title: 신입 사원, Time: 1784 ms, Memory: 8456 KB -BaekjoonHub

--- 백준/Silver/1946. 신입 사원/신입 사원.go
+++ 백준/Silver/1946. 신입 사원/신입 사원.go
@@ -1,10 +1,10 @@
+// 참고한 코드
 package main
 
 import (
 	"bufio"
 	"fmt"
 	"os"
-	"sort"
 )
 
 var (
@@ -17,66 +17,25 @@ func main() {
 
 	var t, n int
 	var a, b int
-	var scores [][]int
+	var scores []int
 
 	fmt.Fscanf(r, "%d\n", &t)
 	for i := 0; i < t; i++ {
 		ans := 0
 		fmt.Fscanf(r, "%d\n", &n)
-		scores = make([][]int, n)
+		scores = make([]int, n+1)
 		for j := 0; j < n; j++ {
 			fmt.Fscanf(r, "%d %d\n", &a, &b)
-			scores[j] = []int{a, b}
+			scores[a] = b
 		}
-		sort.Slice(scores, func(i2, j int) bool {
-			return scores[i2][0] < scores[j][0]
-		})
-		last := scores[0][1]
-		for k := 1; k < n; k++ {
-			if last < scores[k][1] {
+		last := scores[1]
+		for k := 1; k < n+1; k++ {
+			if last < scores[k] {
 				ans++
 			} else {
-				last = scores[k][1]
+				last = scores[k]
 			}
 		}
 		fmt.Fprintln(w, n-ans)
 	}
-}
-
-func mergeSort(arr [][]int) [][]int {
-	if len(arr) <= 1 {
-		return arr
-	}
-
-	mid := len(arr) / 2
-	left := mergeSort(arr[:mid])
-	right := mergeSort(arr[mid:])
-
-	return merge(left, right)
-}
-
-func merge(left, right [][]int) [][]int {
-	ret := make([][]int, len(left)+len(right))
-	l, r := 0, 0
-
-	for {
-		if l < len(left) && r < len(right) {
-			if left[l][0] < right[r][0] {
-				ret[l+r] = left[l]
-				l++
-			} else {
-				ret[l+r] = right[r]
-				r++
-			}
-		} else if l < len(left) {
-			ret[l+r] = left[l]
-			l++
-		} else if r < len(right) {
-			ret[l+r] = right[r]
-			r++
-		} else {
-			break
-		}
-	}
-	return ret
 }

2291b4c670b4f608ea83339767b2f6e0b2734318
ohtjqkd <ohtjqkd@gmail.com>
2023-11-20 19:01:07 +0900

    [Bronze II] Title: 대표값, Time: 72 ms, Memory: 29200 KB -BaekjoonHub

---
+++ 백준/Bronze/2592. 대표값/대표값.py
@@ -0,0 +1,13 @@
+S = 0
+max_freq_cnt, max_freq_num = 0, None
+cnt = {}
+for _ in range(10):
+    n = int(input())
+    S += n
+    cnt[n] = cnt.get(n, 0) + 1
+    if cnt[n] > max_freq_cnt:
+        max_freq_cnt = cnt[n]
+        max_freq_num = n
+
+print(S//10)
+print(max_freq_num)
